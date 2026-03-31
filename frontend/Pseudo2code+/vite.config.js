c8b10f9911b2da75c3564cdf92119072149257fa
Ahamed <muhsinjham@gmail.com>
2026-03-31 22:38:32 +0530

    fix

--- frontend/Pseudo2code+/vite.config.js
+++ frontend/Pseudo2code+/vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/PSEUDO2CODE-plus-AI-POWERED-CODE-GENERATOR/',
   plugins: [
     vue(),
     vueJsx(),
